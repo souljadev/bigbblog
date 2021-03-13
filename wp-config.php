727aee5199526c6850276651473776b7949ac80c
bigb <brendan.moy@gmail.com>
2021-03-13 08:41:06 -0500

    modified wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,17 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
-
-/** MySQL database username */
-define( 'DB_USER', 'username_here' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
-
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
-
-/** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
+    $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+    define('DB_NAME', trim($db['path'],'/'));
+    define('DB_USER', $db['user']);
+    define('DB_PASSWORD', $db['pass']);
+    define('DB_HOST', $db['host']);
+    define('DB_CHARSET', 'utf8');
+    define('DB_COLLATE', '');
+} else {
+    die('No Database credentials!');
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,15 +41,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
 /**#@-*/
 
 /**
